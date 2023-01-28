8d43a0114de1503e92e3b536aa47bb92233e9b04
Nishant Patel <119747129+Nishantptl@users.noreply.github.com>
2023-01-27 22:17:56 -0800

    Add files via upload

---
+++ 1196A_Three_Piles_of_Candies.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    long long int t, a, b, c;
+    cin>>t;
+    while(t>0)
+    {
+        cin>>a>>b>>c;
+        cout<<(a+b+c)/2<<endl;
+        t--;
+    }
+    return 0;
+}
